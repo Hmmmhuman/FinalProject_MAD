15399c8cc05948809797a14674415b077ac46eba
RodrigoWonua <rodrigorojerswonua@gmail.com>
2024-06-30 16:15:19 +0800

    Project

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,5 @@
+import Logo from './Logo.svg';
+import Lts from './lg-LPS.png';
+import Llogo from './logo-livin_prev_ui.png';
+
+export {Logo, Lts, Llogo};
---
+++ src/pages/splashScreen/index.tsx
@@ -0,0 +1,48 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {Llogo} from '../../assets/icons';
+import {Lts} from '../../assets/icons';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={Llogo} style={styles.avatar} />
+      <View style={styles.footer}>
+        <Image source={Lts} style={styles.lts} />
+        <Text style={styles.title}>
+          PT Bank Mandiri (Persero) Tbk.berizin dan diawasi oleh Otoritas Jasa
+          Keuangan (OJK) serta merupakan peserta penjamin Lembaga Penjamin
+          Simpanan(LPS)
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#3060d5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    height: 400,
+    width: 500,
+  },
+  lts: {
+    height: 140,
+    width: 110,
+  },
+  footer: {
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 15,
+    fontFamily: 'Poppins-Medium',
+    color: 'white',
+  },
+});
