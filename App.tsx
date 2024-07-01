05781dfb012584e99c9b0df7b5bca0c3203173cd
RodrigoWonua <rodrigorojerswonua@gmail.com>
2024-07-01 21:11:23 +0800

    splash & sign

--- App.tsx
+++ App.tsx
@@ -2,9 +2,30 @@ import React from 'react';
 import SplashScreen from './src/pages/splashScreen/index';
 import SignUp from './src/pages/SignUp';
 import Sign from './src/pages/Sign';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <Sign />;
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Sign"
+            component={Sign}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 };
 
 export default App;
